5f4c9a440f3d2d367698463e998f0d7b3efc9b60
mickmed <mick2090@gmail.com>
2020-09-10 07:40:39 -0400

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ projects = Project.create!([
     {
         name: 'Mick\'s Math Mental', 
         subtitle: 'Test your mental arithmetic against the timer', 
-        site_url: 'http://micks-mental-math',  
+        site_url: 'http://micks-mental-math.surge.sh',  
         github_url:'https://github.com/mickmed/mental-math-master', 
         img_url:'math-game.png' 
     },
