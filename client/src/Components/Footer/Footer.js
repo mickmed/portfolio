f68976be133c01d5f3e2e3d36e2702aad256fdc4
mickmed <mick2090@gmail.com>
2020-11-17 22:57:27 -0500

    sass version

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ export const Footer = () => {
     'https://www.linkedin.com/in/mick-roth/': '<i class="fab fa-linkedin"></i>',
     'https://www.facebook.com/mick.roth.1': '<i class="fab fa-facebook-square"></i>',
     'https://twitter.com/micky_medium': '<i class="fab fa-twitter"></i>',
-    'https://mail.google.com/mail/?view=cm&fs=1&to=mickrothnyc@gmail.com': '<i class="fa fa-envelope" aria-hidden="true" style="background:transparent;font-size:2em;color:lightBlue"></i>',
+    'https://mail.google.com/mail/?view=cm&fs=1&to=mickrothnyc@gmail.com': '<i class="fa fa-envelope" aria-hidden="true" style="background:transparent;font-size:1em;color:rgb(189, 75, 75)"></i>',
 
    
 
