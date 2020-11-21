08b18869ea84ebce9cc6eaf362fabb54edf44217
mickmed <mick2090@gmail.com>
2020-11-20 20:48:25 -0500

    resume

--- client/src/Components/Resume/ResumePdf.js
+++ client/src/Components/Resume/ResumePdf.js
@@ -18,7 +18,7 @@ export async function Resume() {
 
 
   const resumePdf = `<object class='resume-pdf' data=${PdfFile}  type="application/pdf">
-  <embed src="resume-pdf" type="application/pdf"
+  <embed src="${PdfFile} type="application/pdf"
 
 </object>`
   const img = cecl('img', 'img')
