c7fa8b2a40faae5f58ad94eed9ed7de7557276c6
mickmed <mick2090@gmail.com>
2020-11-20 22:09:53 -0500

    resume

--- client/src/Components/Resume/ResumePdf.js
+++ client/src/Components/Resume/ResumePdf.js
@@ -15,12 +15,14 @@ export async function Resume() {
   while (mainContentScrollable.childNodes.length) {
     mainContentScrollable.removeChild(mainContentScrollable.lastChild)
   }
+//   <object class='resume-pdf' data=${PdfFile}  type='application/pdf'>
+//   <embed class='resume-pdf' src='https://docs.google.com/document/d/1F0CtmV9ViPecK5KDHgKS2BZJiSy8QL6LS51BF2IaroI/edit' type='application/pdf'/>
 
+// </object>
 
   const resumePdf = `
-  <embed class='resume-pdf' src=${PdfFile} type='application/pdf'/>
 
-
+<iframe class='resume-pdf' src=${PdfFile}></iframe>
 
 `
   const img = cecl('img', 'img')
