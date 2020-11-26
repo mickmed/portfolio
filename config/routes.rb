5ccee32140a1124203a399d8cdf365fe430dd72e
mickmed <mick2090@gmail.com>
2020-11-25 22:12:54 -0500

    style project page

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,4 @@ Rails.application.routes.draw do
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
