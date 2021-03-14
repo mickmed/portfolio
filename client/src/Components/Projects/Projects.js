a9aa0c8d958de6958778ce0ad87ccb2247147118
mickmed <mick2090@gmail.com>
2021-03-14 17:54:03 -0400

    fix icons

--- client/src/Components/Projects/Projects.js
+++ client/src/Components/Projects/Projects.js
@@ -1,212 +1,205 @@
 // import "../Shared/Image"
-import { qs, qsa, cecl, ac, btn } from "../Helpers/domHelper"
-import { verify } from "../Services/ApiAuth.js"
-import { Image } from "../Shared/Image.js"
-import { Footer } from "../Footer/Footer.js"
-import {
-  getProjects,
-  deleteProject,
-
-} from "../Services/ApiProject.js"
-import { Form, Input, Button, Checkbox, Label } from "../Shared/Form.js"
-import { EditProject } from "./EditProject"
-import "./projects.scss"
+import { qs, qsa, cecl, ac, btn } from "../Helpers/domHelper";
+import { verify } from "../Services/ApiAuth.js";
+import { Image } from "../Shared/Image.js";
+import { Footer } from "../Footer/Footer.js";
+import { getProjects, deleteProject } from "../Services/ApiProject.js";
+import { Form, Input, Button, Checkbox, Label } from "../Shared/Form.js";
+import { EditProject } from "./EditProject";
+import "./projects.scss";
 
 /**********
  CLEAR PAGE
 ***********/
 const clearPage = (element) => {
   while (element.childNodes.length) {
-    element.removeChild(element.lastChild)
+    element.removeChild(element.lastChild);
   }
-}
+};
 
 export async function Projects() {
-
-  const mainContent = qs('.scrollable-content')
-  clearPage(mainContent)
-  let loading = mainContent.appendChild(cecl("div", "loading"))
-  loading.innerHTML = "loading..."
-  const user = await verify()
-
+  const mainContent = qs(".scrollable-content");
+  clearPage(mainContent);
+  let loading = mainContent.appendChild(cecl("div", "loading"));
+  loading.innerHTML = "loading...";
+  const user = await verify();
 
   /**************
   RENDER PROJECTS
   ***************/
-  const resp = await getProjects()
-  clearPage(mainContent)
+  const resp = await getProjects();
+  clearPage(mainContent);
 
-  const containerWidth = mainContent.clientWidth
+  const containerWidth = mainContent.clientWidth;
   resp.forEach(async (project, index) => {
+    const projectWrap = mainContent.appendChild(cecl("div", "project-wrap"));
 
-    const projectWrap = mainContent.appendChild(cecl("div", "project-wrap"))
-    const image = projectWrap.appendChild(
-      Image(`src/img/${project.img_url}`, project.name, true, project.site_url, project.build_date)
-    )
-   
-    let bool = "false"
-    let linkModal = cecl("div", "link-modal")
-    let imgWrapper = qsa(".img-wrapper")
-    let innerImgWrap = qsa(".inner-img-wrap")
-    let techIcons = cecl("div", "tech-icons")
+    let projectImg = cecl("div", "project-img");
+    let imgWrapper = cecl("div", "img-wrapper");
+
+    projectImg.appendChild(imgWrapper);
+    let innerImgWrap = cecl("div", "inner-img-wrap");
+    imgWrapper.appendChild(innerImgWrap);
+
+    projectWrap.appendChild(projectImg);
+
+    innerImgWrap.appendChild(Image(`src/img/${project.img_url}`, innerImgWrap));
+
+    let modal = innerImgWrap.appendChild(cecl("div", "modal"));
+    modal.classList.add("project-modal", "modal-slide");
+    let modalSpan = modal.appendChild(cecl("span", "modal-span"));
+    let h2 = modal.childNodes[0].appendChild(cecl("h2", "project-title"));
+    h2.innerHTML = project.name;
+    modal.childNodes[0].append(h2);
+    let buildDateStr = modal.childNodes[0].appendChild(
+      cecl("div", "build-date")
+    );
+    buildDateStr.innerHTML = new Date(project.build_date).getFullYear();
+    modal.childNodes[0].append(buildDateStr);
+
+    // console.log(project)
+
+    let bool = "false";
+    let linkModal = cecl("div", "link-modal");
+    // let imgWrapper = qsa(".img-wrapper")
+    // let innerImgWrap = qsa(".inner-img-wrap")
+    let techIcons = cecl("div", "tech-icons");
     project.technologies.forEach((icon) => {
-      let techIcon = techIcons.appendChild(cecl("img", "tech-icon"))
-      techIcon.src = `src/img/${icon.icon_url}`
-    })
-    let description = cecl('div', 'description')
-    description.innerHTML = project.description
+      let techIcon = techIcons.appendChild(cecl("img", "tech-icon"));
+      techIcon.src = `src/img/${icon.icon_url}`;
+    });
+    let description = cecl("div", "description");
+    description.innerHTML = project.description;
 
     if (containerWidth < 900) {
-      linkModal.appendChild(techIcons)
-      linkModal.appendChild(description)
+      linkModal.appendChild(techIcons);
+      linkModal.appendChild(description);
     } else {
-      linkModal.appendChild(description)
-      linkModal.appendChild(techIcons)
+      linkModal.appendChild(description);
+      // innerImgWrap.appendChild(techIcons);
     }
 
-    const linkModalIcons = linkModal.appendChild(cecl('div', 'link-modal-icons'))
+    const linkModalIcons = linkModal.appendChild(
+      cecl("div", "link-modal-icons")
+    );
 
-    linkModalIcons.innerHTML =
-      `<div><a href=${project.site_url} target='_blank'><i class="fas fa-home fa-fw"></i></a>
+    linkModalIcons.innerHTML = `<div><a href=${project.site_url} target='_blank'><i class="fas fa-home fa-fw"></i></a>
     <p class='home-tool-tip'>website</p></div>
 
     <div><a href=${project.github_url} target='_blank'><i class="fab fa-github"></i></a>
-    <p class='github-tool-tip'>github</p></div>`
-
+    <p class='github-tool-tip'>github</p></div>`;
 
     /**************
     ADD LINK MODAL
     ***************/
-    imgWrapper[index].addEventListener("click", async () => {
-      
-     
-     
+    imgWrapper.addEventListener("click", async () => {
       if (bool === "true") {
-        bool = "false"
-        innerImgWrap[index].classList.add("close-curtain")
-        innerImgWrap[index].classList.remove("open-curtain")
+        bool = "false";
+        innerImgWrap.classList.add("close-curtain");
+        innerImgWrap.classList.remove("open-curtain");
         // console.log(lastChild)
-        innerImgWrap[index].lastChild.remove()
-        imgWrapper[index].lastChild.remove()
+        innerImgWrap.lastChild.remove();
+        imgWrapper.lastChild.remove();
 
+        imgWrapper.style.width = "80%";
+        imgWrapper.style.padding = "6%";
+        imgWrapper.style.margin = "0";
 
-        imgWrapper[index].style.width = '80%'
-        imgWrapper[index].style.padding = '6%'
-        imgWrapper[index].style.margin = '0'
+        projectWrap.style.margin = "0 10% 3% 10%";
+        innerImgWrap.style.width = "100%";
 
-        projectWrap.style.margin = '0 10% 3% 10%'
-        innerImgWrap[index].style.width = '100%'
-
-        projectWrap.classList.remove("project-wrap-click")
-
-      
-
+        projectWrap.classList.remove("project-wrap-click");
       } else if (bool === "false") {
-       console.log('true')
-        bool = "true"
-        innerImgWrap[index].classList.add("open-curtain")
-        innerImgWrap[index].classList.remove("close-curtain")
-        imgWrapper[index].appendChild(linkModal)
-        projectWrap.style.margin = '0 5% 5% 5%'
-
-
+        console.log("true");
+        bool = "true";
+        innerImgWrap.classList.add("open-curtain");
+        innerImgWrap.classList.remove("close-curtain");
+        imgWrapper.appendChild(linkModal);
+        projectWrap.style.margin = "0 5% 5% 5%";
 
         if (containerWidth < 900) {
-          imgWrapper[index].style.flexDirection = 'column'
-          linkModal.style.width = '90%'
-          techIcons.style.width = '80%'
-          linkModal.style.marginTop = '5%'
-          linkModal.style.background = 'rgb(240, 253, 253)'
+          imgWrapper[index].style.flexDirection = "column";
+          linkModal.style.width = "90%";
+          techIcons.style.width = "80%";
+          linkModal.style.marginTop = "5%";
+          linkModal.style.background = "rgb(240, 253, 253)";
         } else {
-
-          imgWrapper[index].style.width = '90%'
-          imgWrapper[index].style.padding = '2%'
-          imgWrapper[index].style.margin = '0 auto'
+          imgWrapper.style.width = "90%";
+          imgWrapper.style.padding = "2%";
+          imgWrapper.style.margin = "0 auto";
           // projectWrap.style.margin = '0 1%'
 
-          innerImgWrap[index].style.width = '99%'
-          innerImgWrap[index].style.height = '99%'
-          // 
+          innerImgWrap.style.width = "99%";
+          innerImgWrap.style.height = "99%";
+          //
         }
-
 
         if (containerWidth < 900) {
-          console.log('900-')
-          image.appendChild(techIcons)
-        }
-        else {
-          console.log('900+')
-          innerImgWrap[index].appendChild(techIcons)
-
+          console.log("900-");
+          projectImg.appendChild(techIcons);
+        } else {
+          console.log("900+");
+          innerImgWrap.appendChild(techIcons);
         }
 
-
-
-        console.log(image)
-
-        let iw = imgWrapper[index].style
+        let iw = imgWrapper.style;
         iw.setProperty(
           "--animation",
           "animate-img-wrapper 1.5s linear forwards"
-        )
-        projectWrap.classList.add("project-wrap-click")
-
+        );
+        projectWrap.classList.add("project-wrap-click");
       }
-    })
+    });
 
     /*******************
      EDIT PROJECT BUTTON
     ********************/
-   
-    if (user && user.isAdmin) {
 
-      const editProjectWrapper = cecl("div", "edit-project-wrapper")
-      const editButton = Button("show-edit-button", "submit", "update")
+    if (user && user.isAdmin) {
+      const editProjectWrapper = cecl("div", "edit-project-wrapper");
+      const editButton = Button("show-edit-button", "submit", "update");
       editButton.addEventListener("click", () => {
-        console.log(projectWrap.lastChild.className)
-        if (
-          projectWrap.lastChild.className !== "edit-project-form"
-        )
-          EditProject(projectWrap, project)
+        console.log(projectWrap.lastChild.className);
+        if (projectWrap.lastChild.className !== "edit-project-form")
+          EditProject(projectWrap, project);
         else {
-          projectWrap.lastChild.remove()
+          projectWrap.lastChild.remove();
         }
-      })
-      editProjectWrapper.appendChild(editButton)
+      });
+      editProjectWrapper.appendChild(editButton);
 
       /************************
        DELETE PROJECT BUTTON
       ************************/
-      const deleteButton = Button("delete-project", "submit", "delete")
+      const deleteButton = Button("delete-project", "submit", "delete");
       deleteButton.addEventListener("click", async () => {
-        let answer = confirm(`Are you sure you want to delete ${project.name}?`)
-        answer && await deleteProject(project.id)
-        answer && await Projects()
-      })
-      editProjectWrapper.appendChild(deleteButton)
-      projectWrap.appendChild(editProjectWrapper)
+        let answer = confirm(
+          `Are you sure you want to delete ${project.name}?`
+        );
+        answer && (await deleteProject(project.id));
+        answer && (await Projects());
+      });
+      editProjectWrapper.appendChild(deleteButton);
+      projectWrap.appendChild(editProjectWrapper);
     }
-  })
+  });
 
   /***********
   ADD PROJECT 
   ************/
   if (user && user.isAdmin) {
-
-    let addBtn = Button("show-add-form", "submit", "add project")
+    let addBtn = Button("show-add-form", "submit", "add project");
     addBtn.addEventListener("click", () => {
-      console.log(mainContent.lastChild.previousSibling.className)
+      console.log(mainContent.lastChild.previousSibling.className);
       if (mainContent.lastChild.previousSibling.className === "show-add-form")
-        EditProject(mainContent, resp[0], "addProject")
+        EditProject(mainContent, resp[0], "addProject");
       else {
-        mainContent.lastChild.remove()
+        mainContent.lastChild.remove();
       }
-    })
+    });
 
-    ac(mainContent, addBtn)
+    ac(mainContent, addBtn);
   }
 
-  mainContent.appendChild(Footer())
-
-
+  mainContent.appendChild(Footer());
 }
--- client/src/Components/Shared/Image.js
+++ client/src/Components/Shared/Image.js
@@ -1,87 +1,60 @@
-import { cecl, qs } from "../Helpers/domHelper.js"
-import "./image.scss"
+import { cecl, qs } from "../Helpers/domHelper.js";
+import "./image.scss";
 
+export const Image = (src, div) => {
+  let img = div.appendChild(cecl("img", "img"));
+  img.src = src;
 
-export const Image = (src, title, modal, site_url, buildDate) => {
-  // console.log(src)
-  let projectImg = cecl("div", "project-img")
+  // if (title === 'NYC Trees') {
+  //   // modal.childNodes[0].childNodes[0].style.fontFamily = "Maitree"
 
-  let imgWrapper = cecl("div", "img-wrapper")
-  projectImg.appendChild(imgWrapper)
-  let div = cecl("div", "inner-img-wrap")
-  imgWrapper.appendChild(div)
+  // } else if (title === 'Mick\'s Math Mental') {
+  //   // modal.childNodes[0].childNodes[0].style.fontFamily = 'Simple'
 
+  //   modal.childNodes[0].childNodes[0].style.color = 'rgb(203, 206, 45)'
+  // }else if (title === 'Scaled'){
+  //   // modal.childNodes[0].childNodes[0].style.fontFamily = "Vegan"
 
-  let img = div.appendChild(cecl("img", "img"))
-  img.src = src
+  // }
 
+  // console.log("projecttitle", h2)
+  // if (img.height > img.width) {
+  //   modalSpan.style.transform = "skew(-10deg) translateX(-30%)"
+  //   modalSpan.style.width = '60%'
+  //   h2.style.transform = "skew(10deg) translate(1%)"
+  //   console.log(window.innerWidth)
+  //   window.addEventListener('resize', ()=>{
+  //       if(window.innerWidth > 600){
+  //     imgWrapper.style.width = '40%'
+  //   }else{
+  //     imgWrapper.style.width = '70%'
+  //   }
+  //   })
 
-  if (modal === true) {
-    let modal = div.appendChild(cecl("div", "modal"))
-    modal.classList.add("project-modal", "modal-slide")
-    let modalSpan = modal.appendChild(cecl("span", "modal-span"))
-    let h2 = modal.childNodes[0].appendChild(cecl('h2', 'project-title'))
-    h2.innerHTML = title
-    modal.childNodes[0].append(h2)
-    let buildDateStr = modal.childNodes[0].appendChild(cecl('div', 'build-date'))
-    buildDateStr.innerHTML = new Date(buildDate).getFullYear()
-    modal.childNodes[0].append(buildDateStr)
+  //   // imgWrapper.style.width = '40%'
+  //   // h2.style.
+  // }
 
+  // linkModalMore.addEventListener("click", async evt => {
+  //   let res = await showSingleItemm(evt, e, "projects");
 
-    // if (title === 'NYC Trees') {
-    //   // modal.childNodes[0].childNodes[0].style.fontFamily = "Maitree"
+  //   document.querySelector(".main-content").innerHTML = "";
+  //   navbar({ project: e });
+  //   projects("project", {
+  //     project: e,
+  //     technologies: res.technologies,
+  //     traits: res.traits
+  //   });
+  //   technologies("technologies", { technologies: res.technologies });
+  // });
 
-    // } else if (title === 'Mick\'s Math Mental') {
-    //   // modal.childNodes[0].childNodes[0].style.fontFamily = 'Simple'
+  // let linkModalSiteBtn = makeElement("button", "site-button", linkModal, {
+  //   innerText: "visit site"
+  // });
+  // linkModalSiteBtn.addEventListener("click", evt => {
+  //   location.href = e.site_url;
+  //   target = "_blank";
+  // });
 
-    //   modal.childNodes[0].childNodes[0].style.color = 'rgb(203, 206, 45)'
-    // }else if (title === 'Scaled'){
-    //   // modal.childNodes[0].childNodes[0].style.fontFamily = "Vegan"
-
-    // }
-
-    // console.log("projecttitle", h2)
-    // if (img.height > img.width) {
-    //   modalSpan.style.transform = "skew(-10deg) translateX(-30%)"
-    //   modalSpan.style.width = '60%'
-    //   h2.style.transform = "skew(10deg) translate(1%)"
-    //   console.log(window.innerWidth)
-    //   window.addEventListener('resize', ()=>{
-    //       if(window.innerWidth > 600){
-    //     imgWrapper.style.width = '40%'
-    //   }else{
-    //     imgWrapper.style.width = '70%'
-    //   }
-    //   })
-
-    //   // imgWrapper.style.width = '40%'
-    //   // h2.style.
-    // }
-
-
-
-
-    // linkModalMore.addEventListener("click", async evt => {
-    //   let res = await showSingleItemm(evt, e, "projects");
-
-    //   document.querySelector(".main-content").innerHTML = "";
-    //   navbar({ project: e });
-    //   projects("project", {
-    //     project: e,
-    //     technologies: res.technologies,
-    //     traits: res.traits
-    //   });
-    //   technologies("technologies", { technologies: res.technologies });
-    // });
-
-    // let linkModalSiteBtn = makeElement("button", "site-button", linkModal, {
-    //   innerText: "visit site"
-    // });
-    // linkModalSiteBtn.addEventListener("click", evt => {
-    //   location.href = e.site_url;
-    //   target = "_blank";
-    // });
-  }
-
-  return projectImg
-}
+  return img;
+};
